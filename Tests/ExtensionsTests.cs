a2711864fe44ed6041aa2eb0700bac075cb886b1
DESKTOP-4CDNPFM\Egor <luk_96@mail.ru>
2021-05-17 21:02:57 +0300

    add extension methods for MathParser.TryParse and IExpression.ComputeValue

---
+++ Tests/ExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using EgorLucky.MathParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class ExtensionsTests
+    {
+        private readonly MathParser _parser = new MathParser();
+        [Fact]
+        public void ExtensionTest()
+        {
+            var expression = "x+y+2*z";
+            var x = 0;
+            var y = 20;
+            var z = 1.1;
+
+            var result = _parser.TryParse(expression, "x","y", "z");
+
+            var computedResult = 0d;
+            if (result.IsSuccessfulCreated)
+                computedResult = result.Expression.ComputeValue(x, y, z);
+
+            var expectedResult = x + y + 2 * z;
+
+            Assert.True(result.IsSuccessfulCreated);
+            Assert.Equal("Sum", result.Expression.Name);
+            Assert.Equal(expectedResult, computedResult);
+        }
+    }
+}
